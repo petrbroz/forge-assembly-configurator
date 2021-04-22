16eddcd08c11e9c1b97d649e0921c597c0a74473
Petr Broz <ipetrbroz@gmail.com>
2021-04-22 12:49:57 +0200

    Avoid showing user's name if it's an email address.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { AuthenticationClient } = require('forge-server-utils');
 const { FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, FORGE_CALLBACK_URL } = require('../../config.js');
 
+const EmailRegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 let router = express.Router();
 let authenticationClient = new AuthenticationClient(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET);
 
@@ -26,6 +27,10 @@ router.get('/callback', async function (req, res, next) {
         req.session.refresh_token = token.refresh_token;
         req.session.expires_at = Date.now() + token.expires_in * 1000;
         const profile = await authenticationClient.getUserProfile(req.session.access_token);
+        // Clean up the user name if it's an email
+        if (EmailRegExp.test(profile.userName.toLowerCase())) {
+            profile.userName = profile.userName.substr(0, profile.userName.indexOf('@'));
+        }
         req.session.user_id = profile.userId;
         req.session.user_name = profile.userName;
         res.redirect('/');
